beef6457bc02b144b904cbb37a34ed71129b7a15
bsweat <brandon.sweat1@gmail.com>
2025-05-04 20:07:52 -0600

    v4

--- js/fade.js
+++ js/fade.js
@@ -1,3 +1,9 @@
 window.addEventListener("DOMContentLoaded", () => {
     document.body.classList.add("fade-in");
+
+    const typingElement = document.querySelector('.typing');
+    if (typingElement) {
+        const charCount = typingElement.textContent.length;
+        typingElement.style.setProperty('--chars', charCount);
+    }
 });
