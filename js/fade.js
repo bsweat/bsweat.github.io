bb1a8797d3bd5414ae57876643889f50d466d1ed
bsweat <brandon.sweat1@gmail.com>
2025-05-14 04:05:14 -0600

    Start over

--- js/fade.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener("DOMContentLoaded", () => {
-    document.body.classList.add("fade-in");
-
-    const typingElement = document.querySelector('.typing');
-    if (typingElement) {
-        const charCount = typingElement.textContent.trim().length;
-        typingElement.style.setProperty('--chars', charCount);
-    }
-});
